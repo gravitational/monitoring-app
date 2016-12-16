4052eff862ce98d2c6f6e97132c6bfb91aab9a52
Roman Tkachenko <roman@gravitational.com>
2016-12-15 18:13:11 -0800

    Use admin config for Grafana in dev mode

--- resources/install.sh
+++ resources/install.sh
@@ -3,5 +3,11 @@
                  --namespace=kube-system \
                  --from-literal=username=admin \
                  --from-literal=password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-/opt/bin/kubectl apply \
-                 --filename=/var/lib/gravity/resources/resources.yaml
+
+if [ "$DEVMODE" = "true" ]; then
+    /opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-ini-dev.yaml
+else
+    /opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-ini-prod.yaml
+fi
+
+/opt/bin/kubectl create -f /var/lib/gravity/resources/resources.yaml
