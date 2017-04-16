768bf4ec91d471ad104b587d7ba25a318e10095e
dmitri <deemok@gmail.com>
2017-04-16 21:26:01 +0200

    Encode password placeholder as base64 to comply with Secret data attribute format

--- resources/install.sh
+++ resources/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n ' | /opt/bin/base64)
-sed -i s/password-goes-here/$password/g /var/lib/gravity/resources/grafana-creds.yaml
+sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/grafana-creds.yaml
 /opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-creds.yaml
 
 if [ "$DEVMODE" = "true" ]; then
