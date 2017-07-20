85c3e225c8b091e6d4e91f4dfb6a84e968e5e358
Roman Tkachenko <roman@gravitational.com>
2017-07-20 13:47:53 +0300

    Fix update script to use deployments

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -1,58 +1,64 @@
 #!/bin/sh
 set -e
 
-echo "Assuming changeset from the environment: $RIG_CHANGESET"
+echo "---> Assuming changeset from the environment: $RIG_CHANGESET"
 # note that rig does not take explicit changeset ID
 # taking it from the environment variables
 if [ $1 = "update" ]; then
-    echo "Checking: $RIG_CHANGESET"
+    echo "---> Checking: $RIG_CHANGESET"
     if rig status $RIG_CHANGESET --retry-attempts=1 --retry-period=1s; then exit 0; fi
 
-    echo "Starting update, changeset: $RIG_CHANGESET"
+    echo "---> Starting update, changeset: $RIG_CHANGESET"
     rig cs delete --force -c cs/$RIG_CHANGESET
 
-    echo "Deleting old replication controller 'heapster'"
-    rig delete rc/heapster --resource-namespace=kube-system --force
+    echo "---> Deleting old deployment 'heapster'"
+    rig delete deployments/heapster --resource-namespace=kube-system --force
 
-    echo "Deleting old replication controller 'influxdb'"
-    rig delete rc/influxdb --resource-namespace=kube-system --force
+    echo "---> Deleting old deployment 'influxdb'"
+    rig delete deployments/influxdb --resource-namespace=kube-system --force
 
-    echo "Deleting old replication controller 'grafana'"
-    rig delete rc/grafana --resource-namespace=kube-system --force
+    echo "---> Deleting old deployment 'grafana'"
+    rig delete deployments/grafana --resource-namespace=kube-system --force
 
-    echo "Deleting old configmap 'grafana'"
+    echo "---> Deleting old deployment 'telegraf'"
+    rig delete deployments/telegraf --resource-namespace=kube-system --force
+
+    echo "---> Deleting old deployment 'kapacitor'"
+    rig delete deployments/kapacitor --resource-namespace=kube-system --force
+
+    echo "---> Deleting old configmap 'grafana'"
     rig delete configmaps/grafana --resource-namespace=kube-system --force
 
-    echo "Deleting old secret 'grafana'"
+    echo "---> Deleting old secret 'grafana'"
     rig delete secrets/grafana --resource-namespace=kube-system --force
 
-    echo "Deleting old configmap 'influxdb'"
+    echo "---> Deleting old configmap 'influxdb'"
     rig delete configmaps/influxdb --resource-namespace=kube-system --force
 
-    echo "Creating new configmap 'influxdb'"
+    echo "---> Creating new configmap 'influxdb'"
     rig upsert -f /var/lib/gravity/resources/influxdb-conf.yaml --debug
 
-    echo "Creating new secret 'grafana'"
+    echo "---> Creating new secret 'grafana'"
     password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n ' | /opt/bin/base64)
     sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/grafana-creds.yaml
     rig upsert -f /var/lib/gravity/resources/grafana-creds.yaml --debug
 
-    echo "Creating new configmap 'grafana'"
+    echo "---> Creating new configmap 'grafana'"
     rig upsert -f /var/lib/gravity/resources/grafana-ini.yaml --debug
 
-    echo "Creating or updating resources"
+    echo "---> Creating or updating resources"
     rig upsert -f /var/lib/gravity/resources/resources.yaml --debug
     rig upsert -f /var/lib/gravity/resources/alerts.yaml --debug
 
-    echo "Checking status"
+    echo "---> Checking status"
     rig status $RIG_CHANGESET --retry-attempts=120 --retry-period=1s --debug
 
-    echo "Freezing"
+    echo "---> Freezing"
     rig freeze
 elif [ $1 = "rollback" ]; then
-    echo "Reverting changeset $RIG_CHANGESET"
+    echo "---> Reverting changeset $RIG_CHANGESET"
     rig revert
     rig cs delete --force -c cs/$RIG_CHANGESET
 else
-    echo "Missing argument, should be either 'update' or 'rollback'"
+    echo "---> Missing argument, should be either 'update' or 'rollback'"
 fi
