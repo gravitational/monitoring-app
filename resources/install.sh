903a69589aebb01b8f7ce1b1eb92427e255a862b
Emanuele 'Lele' Calo <lele@gravitational.com>
2018-05-02 19:12:29 +0200

    Fixed install.sh script

--- resources/install.sh
+++ resources/install.sh
@@ -3,6 +3,6 @@ password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -
 sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/grafana-creds.yaml
 /opt/bin/kubectl create -f /var/lib/gravity/resources/influxdb-conf.yaml
 /opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-creds.yaml
-/opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-ini.yaml
+/opt/bin/kubectl create -f /var/lib/gravity/resources/grafana-cfg.yaml
 /opt/bin/kubectl create -f /var/lib/gravity/resources/resources.yaml
 /opt/bin/kubectl create -f /var/lib/gravity/resources/alerts.yaml
