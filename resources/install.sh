7e6542dcbc6a5977722bd59c7ffd535924d41f4f
Sergei Antipov <greendayonfire@gmail.com>
2019-03-26 21:52:47 -0400

    Add prometheus operator

--- resources/install.sh
+++ resources/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n ' | /opt/bin/base64)
-sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/grafana.yaml
 
 /opt/bin/kubectl apply -f /var/lib/gravity/resources/namespace.yaml
 for name in security smtp grafana metrics-server alerts kube-state-metrics
 do
-    /opt/bin/kubectl create -f /var/lib/gravity/resources/${name}.yaml
+    /opt/bin/kubectl apply -f /var/lib/gravity/resources/${name}.yaml
 done
+
+/opt/bin/kubectl apply -f /var/lib/gravity/resources/prometheus/
