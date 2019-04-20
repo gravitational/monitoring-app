9e406218a786725c1632a5c87ad40678727e1ef6
Sergei Antipov <greendayonfire@gmail.com>
2019-04-20 17:25:32 -0400

    Delete kube-state-metrics and metrics server

--- resources/install.sh
+++ resources/install.sh
@@ -7,7 +7,7 @@ do
     head -n -6 $file | /opt/bin/kubectl apply -f -
 done
 
-for name in security smtp grafana metrics-server alerts kube-state-metrics
+for name in security smtp grafana alerts
 do
     /opt/bin/kubectl apply -f /var/lib/gravity/resources/${name}.yaml
 done
