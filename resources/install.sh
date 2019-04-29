ba0b707c049d4851dca664bb78510b9e46ff1a62
Sergei Antipov <greendayonfire@gmail.com>
2019-04-28 21:27:21 -0400

    Add tolerations/affinity to alertmanager. Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-/opt/bin/kubectl apply -f /var/lib/gravity/resources/namespace.yaml
+/opt/bin/kubectl create -f /var/lib/gravity/resources/namespace.yaml
 
 for file in /var/lib/gravity/resources/crds/*
 do
     head -n -6 $file | /opt/bin/kubectl apply -f -
 done
 
-for name in security smtp grafana alerts
+for name in security smtp grafana
 do
-    /opt/bin/kubectl apply -f /var/lib/gravity/resources/${name}.yaml
+    /opt/bin/kubectl create -f /var/lib/gravity/resources/${name}.yaml
 done
 
-/opt/bin/kubectl apply -f /var/lib/gravity/resources/prometheus/
+/opt/bin/kubectl create -f /var/lib/gravity/resources/prometheus/
