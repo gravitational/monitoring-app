823bcb4dfc822038f67acea8773bc4ec0eb6019b
Sergei Antipov <greendayonfire@gmail.com>
2019-07-19 22:51:09 -0400

    Fix usage of secrets in telegraf and grafana

--- images/grafana/run.sh
+++ images/grafana/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-set -m
+set -o errexit
+set -o nounset
+
+echo "Substitute env variables in influxdb datasource"
+mkdir -p /etc/grafana/provisioning/datasources
+cat /etc/grafana/datasources/influxdb.yaml | envsubst > /etc/grafana/provisioning/datasources/influxdb.yaml
+
 echo "Starting Grafana"
 exec /usr/share/grafana/bin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/cfg/grafana.ini
