91eda94df1dcb0a5956030f2b6a185333468f0bc
Sergei Antipov <greendayonfire@gmail.com>
2019-07-20 21:14:43 -0400

    Update grafana image and resources

--- images/grafana/run.sh
+++ images/grafana/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-
-echo "Substitute env variables in influxdb datasource"
-mkdir -p /etc/grafana/provisioning/datasources
-cat /etc/grafana/datasources/influxdb.yaml | envsubst > /etc/grafana/provisioning/datasources/influxdb.yaml
-
+set -m
 echo "Starting Grafana"
 exec /usr/share/grafana/bin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/cfg/grafana.ini
