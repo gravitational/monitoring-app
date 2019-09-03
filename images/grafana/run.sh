b7168f2c7ca5be8716f469d1ce538320b3e19495
Sergei Antipov <greendayonfire@gmail.com>
2019-09-03 11:23:40 -0400

    Update grafana Dockerfile from upstream

--- images/grafana/run.sh
+++ images/grafana/run.sh
@@ -2,6 +2,5 @@
 
 set -m
 echo "Starting Grafana"
-mkdir -p /etc/grafana/provisioning/datasources && \
 /usr/bin/envsubst /etc/grafana/provisioning-noenv/datasources/influxdb.yaml > /tmp/conf && mv /tmp/conf /etc/grafana/provisioning/datasources/influxdb.yaml && \
-exec /usr/share/grafana/bin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/cfg/grafana.ini
+exec /usr/share/grafana/bin/grafana-server --config=/etc/grafana/cfg/grafana.ini
