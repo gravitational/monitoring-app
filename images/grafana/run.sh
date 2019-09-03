b654d38983e5787651212836faf095cd748679bc
Sergei Antipov <greendayonfire@gmail.com>
2019-09-03 12:52:54 -0400

    Fix grafana run script

--- images/grafana/run.sh
+++ images/grafana/run.sh
@@ -2,5 +2,5 @@
 
 set -m
 echo "Starting Grafana"
-/usr/bin/envsubst /etc/grafana/provisioning-noenv/datasources/influxdb.yaml > /tmp/conf && mv /tmp/conf /etc/grafana/provisioning/datasources/influxdb.yaml && \
+cat /etc/grafana/provisioning-noenv/datasources/influxdb.yaml | envsubst > /etc/grafana/provisioning/datasources/influxdb.yaml && \
 exec /usr/share/grafana/bin/grafana-server --config=/etc/grafana/cfg/grafana.ini
