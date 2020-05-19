9440224d319a8683fac8d83924d08bcb6c2dcda4
Sergei Antipov <greendayonfire@gmail.com>
2020-05-19 15:34:30 -0400

    Remove all utils related code

--- images/utils/influxdb_data_migration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -d ${OLD_DATA_DIR}/influxdb ]; then 
-  cp -a ${OLD_DATA_DIR}/influxdb/* ${NEW_DATA_DIR}/
-  ls -and $NEW_DATA_DIR | awk '{system("chown -R "$3":"$4" $NEW_DATA_DIR/*")}'
-  echo "Data migrated."
-fi
