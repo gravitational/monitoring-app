e76254611ee35218ded87617937ca79413263417
Sergei Antipov <sergei@gravitational.com>
2020-07-21 10:01:09 -0400

    Sync kube-prometheus resources with 0.4.x upstream branch` (#183)
    * Synchronize kube-prometheus resources with 0.4.x branch
    
    * Update configmap-reload image
    
    * Use kubectl create
    
    * Fix image name for configmap-reload
    
    * Add gravity cluster related rules/alerts
    
    * Update dashboards
    
    * Update dashboards
    
    * Updated nodes and pods dashboards
    
    * Apply namespace creation
    
    * Change labels for prometheu-operator
    
    * Fix issue with immutable label
    
    * Replace RoleBindingList with raw resources
    
    * Patch resources to delete previous configuration
    
    * Replace RoleList with Roles

--- resources/install.sh
+++ resources/install.sh
@@ -2,10 +2,13 @@
 
 /opt/bin/kubectl apply -f /var/lib/gravity/resources/namespace.yaml
 
-for file in /var/lib/gravity/resources/crds/*
+for file in /var/lib/gravity/resources/kube-prometheus-setup/*
 do
-    head -n -6 $file | /opt/bin/kubectl apply -f -
+    /opt/bin/kubectl create -f $file
 done
+
+# Wait until setup of CRDs are done
+until /opt/bin/kubectl get servicemonitors --all-namespaces ; do echo $(date) ": Waiting for CRDs to setup"; done
 
 # Generate password for Grafana administrator
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n ' | /opt/bin/base64)
@@ -13,8 +16,8 @@ sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/gra
 
 for name in security grafana watcher
 do
-    /opt/bin/kubectl apply -f /var/lib/gravity/resources/${name}.yaml
+    /opt/bin/kubectl create -f /var/lib/gravity/resources/${name}.yaml
 done
 
-/opt/bin/kubectl apply -f /var/lib/gravity/resources/prometheus/
-/opt/bin/kubectl apply -f /var/lib/gravity/resources/nethealth/
+/opt/bin/kubectl create -f /var/lib/gravity/resources/prometheus/
+/opt/bin/kubectl create -f /var/lib/gravity/resources/nethealth/
