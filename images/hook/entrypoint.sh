fd1f6227ad4a0d4a67224e901385c82356f0a295
Sergei Antipov <sergei@gravitational.com>
2020-12-23 15:22:35 -0500

    Fix wait conditioning for update hook

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -91,7 +91,7 @@ if [ $1 = "update" ]; then
 	kubectl --namespace monitoring patch alertmanagers.monitoring.coreos.com main --type=json -p='[{"op": "replace", "path": "/spec/replicas", "value": 2}]'
     fi
     # check for readiness of prometheus pod
-    timeout 5m sh -c "while ! /usr/local/bin/kubectl get job stolon-postgres-hardening; do sleep 10; done"
+    timeout 5m sh -c "while ! /opt/bin/kubectl get pod prometheus-k8s-0; do sleep 10; done"
     kubectl --namespace monitoring wait --for=condition=ready pod prometheus-k8s-0
 
     # Remove unused nethealth objects
