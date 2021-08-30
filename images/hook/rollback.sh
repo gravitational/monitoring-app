924ca3f53c8c97d0d0ec9087a2b74764963f5963
Sergei Antipov <greendayonfire@gmail.com>
2021-08-30 18:56:10 -0400

    Update rollback hook

--- images/hook/rollback.sh
+++ images/hook/rollback.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -eux
-
-echo "---> Reverting changeset $RIG_CHANGESET"
+# TODO (s.antipov): write rollback for Helm to Helm release
 for release_name in monitoring watcher nethealth; do
     if helm3 --namespace monitoring status "$release_name" &>/dev/null; then
         helm3 --namespace monitoring uninstall "$release_name"
     fi
 done
+
+echo "---> Reverting changeset $RIG_CHANGESET"
 rig revert
 rig cs delete --force -c cs/"$RIG_CHANGESET"
